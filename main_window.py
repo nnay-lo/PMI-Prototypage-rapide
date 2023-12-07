16c071f9a65874cb7be6afd057d9d79deac1ea8a
Luc-Sauleau <luc.sauleau@ipsa.fr>
2023-12-07 19:31:46 +0100

    First data and plot integration

--- main_window.py
+++ main_window.py
@@ -3,22 +3,103 @@ import os
 import sys
 
 from PySide6.QtCore import QDir
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QVBoxLayout
+
 
 import sqlite3
 import pandas as pd
-import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import numpy as np
 from sklearn.metrics import r2_score
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
+from scipy.signal import argrelextrema
+import random
+
+#import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 # Important:
 # You need to run the following command to generate the ui_form.py file
 #     pyside6-uic form.ui -o ui_form.py, or
 #     pyside2-uic form.ui -o ui_form.py
 from ui_form import Ui_Main_Window
+
+
+# External Function :
+
+def choose_best_regression(x, y):
+    # Define the models
+    def linear_func(x, a, b):
+        return a * x + b
+
+    def exponential_func(x, a, b, c):
+        return a * np.exp(b * x) + c
+
+
+
+    # Fit the linear model
+    popt_linear, _ = curve_fit(linear_func, x, y)
+    y_pred_linear = linear_func(x, *popt_linear)
+    r2_linear = r2_score(y, y_pred_linear)
+
+
+
+    # Fit the exponential model
+    try:
+        popt_exponential, _ = curve_fit(exponential_func, x, y)
+        y_pred_exponential = exponential_func(x, *popt_exponential)
+        r2_exponential = r2_score(y, y_pred_exponential)
+    except:
+        r2_exponential = -np.inf
+
+    # Compare R-squared values
+    if r2_linear > r2_exponential:
+        return 'Linear Regression', popt_linear, r2_linear, y_pred_linear
+    else:
+        return 'Exponential Regression', popt_exponential, r2_exponential, y_pred_exponential
+
+
+
+
+def simulate_hourly_problems_linear(hour, a, b):
+    probability = a * hour + b
+    if random.random() < probability:
+        return 1
+    else:
+        return 0
+
+def simulate_hourly_problems_exponential(hour, a, b, c):
+    probability = a * np.exp(b * hour) + c
+    if random.random() < probability:
+        return 1
+    else:
+        return 0
+
+def simulate_hours_to_failure(threshold, probability_accept, regression_type, regression_params):
+    # Simulate hours until failure
+    total_problems = 0
+    hours = 1000  # You can adjust the number of hours to simulate
+
+    for hour in range(hours):
+        if regression_type == 'Linear Regression':
+            probability_failure = simulate_hourly_problems_linear(hour, *regression_params)
+        elif regression_type == 'Exponential Regression':
+            probability_failure = simulate_hourly_problems_exponential(hour, *regression_params)
+        else:
+            raise ValueError("Invalid regression type")
+
+        total_problems += probability_failure
+
+    # Check if the part needs to be changed
+    if total_problems > threshold:
+        print("The part needs to be changed.")
+    else:
+        print("The part does not need to be changed at this time.")
+
+
+# QT INTERFACE :
 
 class Main_Window(QMainWindow):
     def __init__(self, parent=None):
@@ -32,6 +113,22 @@ class Main_Window(QMainWindow):
 
         self.ui.browseButton.clicked.connect(self.browse_file)
 
+        # Set up the matplotlib figure and canvas
+        self.figure = Figure()
+        self.canvas = FigureCanvas(self.figure)
+        self.ui.plotWidget.setLayout(QVBoxLayout())
+        self.ui.plotWidget.layout().addWidget(self.canvas)
+
+        self.figure2 = Figure()
+        self.canvas2 = FigureCanvas(self.figure2)
+        self.ui.plotWidget_hist.setLayout(QVBoxLayout())
+        self.ui.plotWidget_hist.layout().addWidget(self.canvas2)
+
+        self.figure3 = Figure()
+        self.canvas3 = FigureCanvas(self.figure3)
+        self.ui.plotWidget_hist2.setLayout(QVBoxLayout())
+        self.ui.plotWidget_hist2.layout().addWidget(self.canvas3)
+
     def browse_file(self):
         options = QFileDialog.Options()
         options |= QFileDialog.ReadOnly
@@ -39,7 +136,37 @@ class Main_Window(QMainWindow):
         file_name, _ = QFileDialog.getOpenFileName(self, 'Select a .db file', os.path.dirname(os.path.abspath(__file__))+"/Databases", 'SQLite Database Files (*.db);;All Files (*)', options=options)
 
         if file_name:
-            self.connect_to_database(file_name)
+            #info = self.get_database_info(file_name)
+            #print(info["Error"])
+            #print(info["Size"])
+            self.ui.fileName.setText(file_name.split("Databases/")[1])
+            self.get_linear_regression(file_name)
+            self.get_occurency(file_name)
+
+    def get_database_info(self, db_file):
+            try:
+                file_size = os.path.getsize(db_file)
+                conn = sqlite3.connect(db_file)
+                cursor = conn.cursor()
+
+                cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+                table_names = [table[0] for table in cursor.fetchall()]
+
+                table_info = {}
+                for table_name in table_names:
+                    cursor.execute(f"PRAGMA table_info({table_name});")
+                    columns = cursor.fetchall()
+                    num_columns = len(columns)
+
+                    cursor.execute(f"SELECT COUNT(*) FROM {table_name};")
+                    num_rows = cursor.fetchone()[0]
+
+                    table_info[table_name] = {"rows": num_rows, "columns": num_columns}
+
+                conn.close()
+                return {"Size": file_size, "Tables": table_names, "Info": table_info}
+            except Exception as e:
+                return {"Error": str(e)}
 
     def get_table_name(self, file_name):
         try:
@@ -95,6 +222,133 @@ class Main_Window(QMainWindow):
         except sqlite3.Error as e:
             print("Error connecting to the database:", e)
 
+
+    def get_occurency(self, file_name):
+        conn = sqlite3.connect(file_name)
+
+        # Query to extract data for occurrences
+        query_occurrences = "SELECT FH, COUNT(*) as num_occurrences FROM Occurrences GROUP BY FH"
+        df = pd.read_sql_query(query_occurrences, conn)
+
+        # Query to extract data for occurrences that led to part replacement
+        query_occurrences_repl = "SELECT FH, COUNT(*) as num_occurrences FROM Occurrences WHERE Troubleshootingréalisé = 'part replacement' GROUP BY FH"
+        df2 = pd.read_sql_query(query_occurrences_repl, conn)
+
+        # Creating intervals of 5000 flight hours
+        df['FH_interval'] = (df['FH'] // 5000) * 5000
+        interval_data = df.groupby('FH_interval')['num_occurrences'].sum().reset_index()
+        interval_data['cumulative_occurrences'] = interval_data['num_occurrences'].cumsum()
+
+        df2['FH_interval'] = (df2['FH'] // 5000) * 5000
+        interval_data_rep = df2.groupby('FH_interval')['num_occurrences'].sum().reset_index()
+        interval_data_rep['cumulative_occurrences'] = interval_data_rep['num_occurrences'].cumsum()
+
+        # Linear regression model
+        mid_points = interval_data['FH_interval'] + 2500
+        choice, popt, r2, y_pred = choose_best_regression(mid_points, interval_data['cumulative_occurrences'])
+
+        # Find local maxima
+        occurrences_array = interval_data['num_occurrences'].values
+        local_max_indices = argrelextrema(occurrences_array, np.greater)[0]
+        local_max_intervals = interval_data['FH_interval'].iloc[local_max_indices]
+
+        occurrences_rep_array = interval_data_rep['num_occurrences'].values
+        local_max_indices_rep = argrelextrema(occurrences_rep_array, np.greater)[0]
+        local_max_intervals_rep = interval_data_rep['FH_interval'].iloc[local_max_indices_rep]
+
+        # Plotting
+        # Clear the existing figure
+        self.figure2.clear()
+        self.figure3.clear()
+
+        # Create two subplots (axes) on the figure
+        ax1 = self.figure2.add_subplot(111)
+        ax2 = self.figure3.add_subplot(111)
+
+        # Create interval labels for both datasets
+        interval_labels = [f'{int(interval)}-{int(interval) + 5000}' for interval in interval_data['FH_interval']]
+        interval_labels2 = [f'{int(interval)}-{int(interval) + 5000}' for interval in interval_data_rep['FH_interval']]
+
+        # Plot the first bar chart
+        ax1.bar(interval_labels, interval_data['num_occurrences'], color='green')
+        ax1.set_xlabel('Total Flight Hours Interval')
+        ax1.set_ylabel('Number of Occurrences')
+        ax1.set_title('Occurrences in Each 5000 Flight Hours Interval')
+        ax1.set_xticklabels(interval_labels, rotation=45, ha='right')
+
+        # Plot the second bar chart
+        ax2.bar(interval_labels2, interval_data_rep['num_occurrences'], color='red')
+        ax2.set_xlabel('Total Flight Hours Interval')
+        ax2.set_ylabel('Number of Occurrences that led to a replacement of the part')
+        ax2.set_title('Occurrences in Each 5000 Flight Hours Interval')
+        ax2.set_xticklabels(interval_labels2, rotation=45, ha='right')
+
+        # Annotate local maxima on the second plot
+        for idx in local_max_indices_rep:
+            ax2.annotate('Max', xy=(interval_labels2[idx], occurrences_rep_array[idx]),
+                         xytext=(0, 10), textcoords="offset points",
+                         ha='center', va='bottom', arrowprops=dict(facecolor='black', shrink=0.05))
+
+        self.figure2.tight_layout()
+        self.figure3.tight_layout()
+
+        # Redraw the canvas with the new plots
+        self.canvas2.draw()
+        self.canvas3.draw()
+
+
+        # Print out the intervals that are local maxima
+        print("Flight hours intervals that are local maximums (Occurrences):", local_max_intervals.tolist())
+        print("Flight hours intervals that are local maximums (Occurrences with part replacement):", local_max_intervals_rep.tolist())
+
+    def get_linear_regression(self, file_name):
+        conn = sqlite3.connect(file_name)
+
+        # Query to extract data for occurrences
+        query_occurrences = "SELECT FH, COUNT(*) as num_occurrences FROM Occurrences GROUP BY FH"
+        df = pd.read_sql_query(query_occurrences, conn)
+
+        # You can set your threshold value
+        threshold = 1000
+        probability_accept = 0.9  # Set your acceptable probability here
+
+        # Extract x and y values for regression
+        x = df['FH'].values
+        y = df['num_occurrences'].values
+        print(y)
+        print("\n len x", len(x))
+        print("\n len y", len(y))
+
+        somme_occur=[0]
+
+        for i in range(len(y)-1):
+            somme_occur.append(somme_occur[i]+ y[i])
+        print(somme_occur)
+        print("\n len occur", len(somme_occur))
+
+
+        # Choose the best regression model
+        regression_type, regression_params, r2, y_pred = choose_best_regression(x, somme_occur)
+
+        # Print the chosen regression model and its parameters
+        print(f"Chosen Regression Model: {regression_type}")
+        print(f"Regression Parameters: {regression_params}")
+        print(f"R-squared: {r2}")
+
+        # Simulate hours until failure based on the chosen regression model
+        simulate_hours_to_failure(threshold, probability_accept, regression_type, regression_params)
+
+        ax = self.figure.add_subplot(111)
+        ax.clear()  # Clear existing data/plots
+        ax.scatter(x, somme_occur)
+        ax.plot(x, y_pred)
+
+        # Update the canvas
+        self.figure.tight_layout()
+        self.canvas.draw()
+
+        # Close database connection
+        conn.close()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- ui_form.py
+++ ui_form.py
@@ -16,22 +16,26 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QComboBox, QFormLayout, QFrame,
-    QGraphicsView, QHBoxLayout, QLabel, QMainWindow,
+    QGridLayout, QHBoxLayout, QLabel, QMainWindow,
     QMenuBar, QPushButton, QScrollArea, QSizePolicy,
-    QStatusBar, QToolBox, QVBoxLayout, QWidget)
+    QStatusBar, QTabWidget, QToolBox, QVBoxLayout,
+    QWidget)
 
 class Ui_Main_Window(object):
     def setupUi(self, Main_Window):
         if not Main_Window.objectName():
             Main_Window.setObjectName(u"Main_Window")
-        Main_Window.resize(1030, 636)
+        Main_Window.resize(1176, 754)
+        icon = QIcon()
+        icon.addFile(u"Icon.png", QSize(), QIcon.Normal, QIcon.Off)
+        Main_Window.setWindowIcon(icon)
         self.centralwidget = QWidget(Main_Window)
         self.centralwidget.setObjectName(u"centralwidget")
         self.horizontalLayout = QHBoxLayout(self.centralwidget)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.frame = QFrame(self.centralwidget)
         self.frame.setObjectName(u"frame")
-        self.frame.setMaximumSize(QSize(340, 16777215))
+        self.frame.setMaximumSize(QSize(360, 16777215))
         self.frame.setFrameShape(QFrame.StyledPanel)
         self.frame.setFrameShadow(QFrame.Raised)
         self.horizontalLayout_2 = QHBoxLayout(self.frame)
@@ -41,14 +45,14 @@ class Ui_Main_Window(object):
         self.scrollArea.setWidgetResizable(True)
         self.scrollAreaWidgetContents = QWidget()
         self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 318, 552))
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 338, 670))
         self.verticalLayout_3 = QVBoxLayout(self.scrollAreaWidgetContents)
         self.verticalLayout_3.setObjectName(u"verticalLayout_3")
         self.toolBox = QToolBox(self.scrollAreaWidgetContents)
         self.toolBox.setObjectName(u"toolBox")
         self.page = QWidget()
         self.page.setObjectName(u"page")
-        self.page.setGeometry(QRect(0, 0, 300, 441))
+        self.page.setGeometry(QRect(0, 0, 320, 559))
         self.horizontalLayout_3 = QHBoxLayout(self.page)
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
         self.frame_3 = QFrame(self.page)
@@ -65,23 +69,23 @@ class Ui_Main_Window(object):
         self.frame_7.setFrameShadow(QFrame.Raised)
         self.formLayout = QFormLayout(self.frame_7)
         self.formLayout.setObjectName(u"formLayout")
-        self.label = QLabel(self.frame_7)
-        self.label.setObjectName(u"label")
-        self.label.setMinimumSize(QSize(150, 0))
+        self.fileName = QLabel(self.frame_7)
+        self.fileName.setObjectName(u"fileName")
+        self.fileName.setMinimumSize(QSize(150, 0))
 
-        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.label)
+        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.fileName)
 
         self.browseButton = QPushButton(self.frame_7)
         self.browseButton.setObjectName(u"browseButton")
         self.browseButton.setMaximumSize(QSize(100, 16777215))
-        icon = QIcon()
+        icon1 = QIcon()
         iconThemeName = u"folder"
         if QIcon.hasThemeIcon(iconThemeName):
-            icon = QIcon.fromTheme(iconThemeName)
+            icon1 = QIcon.fromTheme(iconThemeName)
         else:
-            icon.addFile(u".", QSize(), QIcon.Normal, QIcon.Off)
+            icon1.addFile(u".", QSize(), QIcon.Normal, QIcon.Off)
         
-        self.browseButton.setIcon(icon)
+        self.browseButton.setIcon(icon1)
 
         self.formLayout.setWidget(0, QFormLayout.FieldRole, self.browseButton)
 
@@ -94,7 +98,7 @@ class Ui_Main_Window(object):
         self.frame_6.setFrameShadow(QFrame.Raised)
         self.label_2 = QLabel(self.frame_6)
         self.label_2.setObjectName(u"label_2")
-        self.label_2.setGeometry(QRect(10, 10, 67, 17))
+        self.label_2.setGeometry(QRect(10, 10, 261, 17))
 
         self.verticalLayout_2.addWidget(self.frame_6)
 
@@ -124,7 +128,7 @@ class Ui_Main_Window(object):
         self.toolBox.addItem(self.page, u"Selection des Donn\u00e9es")
         self.page_3 = QWidget()
         self.page_3.setObjectName(u"page_3")
-        self.page_3.setGeometry(QRect(0, 0, 300, 441))
+        self.page_3.setGeometry(QRect(0, 0, 320, 559))
         self.horizontalLayout_4 = QHBoxLayout(self.page_3)
         self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
         self.frame_4 = QFrame(self.page_3)
@@ -134,21 +138,21 @@ class Ui_Main_Window(object):
         self.pushButton_2 = QPushButton(self.frame_4)
         self.pushButton_2.setObjectName(u"pushButton_2")
         self.pushButton_2.setGeometry(QRect(10, 20, 241, 25))
-        icon1 = QIcon()
+        icon2 = QIcon()
         iconThemeName = u"view-refresh"
         if QIcon.hasThemeIcon(iconThemeName):
-            icon1 = QIcon.fromTheme(iconThemeName)
+            icon2 = QIcon.fromTheme(iconThemeName)
         else:
-            icon1.addFile(u".", QSize(), QIcon.Normal, QIcon.Off)
+            icon2.addFile(u".", QSize(), QIcon.Normal, QIcon.Off)
         
-        self.pushButton_2.setIcon(icon1)
+        self.pushButton_2.setIcon(icon2)
 
         self.horizontalLayout_4.addWidget(self.frame_4)
 
         self.toolBox.addItem(self.page_3, u"Propri\u00e9t\u00e9s du Graphique")
         self.page_2 = QWidget()
         self.page_2.setObjectName(u"page_2")
-        self.page_2.setGeometry(QRect(0, 0, 300, 441))
+        self.page_2.setGeometry(QRect(0, 0, 320, 559))
         self.horizontalLayout_5 = QHBoxLayout(self.page_2)
         self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
         self.frame_5 = QFrame(self.page_2)
@@ -180,8 +184,14 @@ class Ui_Main_Window(object):
 
         self.pushButton_3 = QPushButton(self.frame_5)
         self.pushButton_3.setObjectName(u"pushButton_3")
-        icon2 = QIcon(QIcon.fromTheme(u"appointment-new"))
-        self.pushButton_3.setIcon(icon2)
+        icon3 = QIcon()
+        iconThemeName = u"appointment-new"
+        if QIcon.hasThemeIcon(iconThemeName):
+            icon3 = QIcon.fromTheme(iconThemeName)
+        else:
+            icon3.addFile(u".", QSize(), QIcon.Normal, QIcon.Off)
+        
+        self.pushButton_3.setIcon(icon3)
 
         self.verticalLayout_4.addWidget(self.pushButton_3)
 
@@ -199,24 +209,45 @@ class Ui_Main_Window(object):
 
         self.horizontalLayout.addWidget(self.frame)
 
-        self.frame_2 = QFrame(self.centralwidget)
-        self.frame_2.setObjectName(u"frame_2")
-        self.frame_2.setFrameShape(QFrame.StyledPanel)
-        self.frame_2.setFrameShadow(QFrame.Raised)
-        self.verticalLayout = QVBoxLayout(self.frame_2)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.graphicsView = QGraphicsView(self.frame_2)
-        self.graphicsView.setObjectName(u"graphicsView")
+        self.tabWidget = QTabWidget(self.centralwidget)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.tab_2 = QWidget()
+        self.tab_2.setObjectName(u"tab_2")
+        self.gridLayout_2 = QGridLayout(self.tab_2)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.plotWidget_hist = QWidget(self.tab_2)
+        self.plotWidget_hist.setObjectName(u"plotWidget_hist")
 
-        self.verticalLayout.addWidget(self.graphicsView)
+        self.gridLayout_2.addWidget(self.plotWidget_hist, 0, 0, 1, 1)
 
+        self.tabWidget.addTab(self.tab_2, "")
+        self.tab_3 = QWidget()
+        self.tab_3.setObjectName(u"tab_3")
+        self.gridLayout_3 = QGridLayout(self.tab_3)
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.plotWidget_hist2 = QWidget(self.tab_3)
+        self.plotWidget_hist2.setObjectName(u"plotWidget_hist2")
 
-        self.horizontalLayout.addWidget(self.frame_2)
+        self.gridLayout_3.addWidget(self.plotWidget_hist2, 0, 0, 1, 1)
+
+        self.tabWidget.addTab(self.tab_3, "")
+        self.tab = QWidget()
+        self.tab.setObjectName(u"tab")
+        self.gridLayout = QGridLayout(self.tab)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.plotWidget = QWidget(self.tab)
+        self.plotWidget.setObjectName(u"plotWidget")
+
+        self.gridLayout.addWidget(self.plotWidget, 0, 0, 1, 1)
+
+        self.tabWidget.addTab(self.tab, "")
+
+        self.horizontalLayout.addWidget(self.tabWidget)
 
         Main_Window.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(Main_Window)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 1030, 22))
+        self.menubar.setGeometry(QRect(0, 0, 1176, 22))
         Main_Window.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(Main_Window)
         self.statusbar.setObjectName(u"statusbar")
@@ -225,27 +256,31 @@ class Ui_Main_Window(object):
         self.retranslateUi(Main_Window)
 
         self.toolBox.setCurrentIndex(0)
+        self.tabWidget.setCurrentIndex(0)
 
 
         QMetaObject.connectSlotsByName(Main_Window)
     # setupUi
 
     def retranslateUi(self, Main_Window):
-        Main_Window.setWindowTitle(QCoreApplication.translate("Main_Window", u"Main_Window", None))
-        self.label.setText(QCoreApplication.translate("Main_Window", u"file name", None))
-        self.browseButton.setText(QCoreApplication.translate("Main_Window", u"Browse", None))
-        self.label_2.setText(QCoreApplication.translate("Main_Window", u"File Info", None))
+        Main_Window.setWindowTitle(QCoreApplication.translate("Main_Window", u"Predictive Maintenance", None))
+        self.fileName.setText(QCoreApplication.translate("Main_Window", u"nom du fichier", None))
+        self.browseButton.setText(QCoreApplication.translate("Main_Window", u"Rechercher", None))
+        self.label_2.setText(QCoreApplication.translate("Main_Window", u"Information sur le fichier :", None))
         self.comboBox.setCurrentText("")
         self.comboBox.setPlaceholderText(QCoreApplication.translate("Main_Window", u"Flotte", None))
         self.comboBox_2.setPlaceholderText(QCoreApplication.translate("Main_Window", u"Equippement", None))
         self.comboBox_4.setCurrentText("")
-        self.comboBox_4.setPlaceholderText(QCoreApplication.translate("Main_Window", u"Part number", None))
+        self.comboBox_4.setPlaceholderText(QCoreApplication.translate("Main_Window", u"Num\u00e9ro de la pi\u00e8ce", None))
         self.toolBox.setItemText(self.toolBox.indexOf(self.page), QCoreApplication.translate("Main_Window", u"Selection des Donn\u00e9es", None))
         self.pushButton_2.setText(QCoreApplication.translate("Main_Window", u"Actualiser", None))
         self.toolBox.setItemText(self.toolBox.indexOf(self.page_3), QCoreApplication.translate("Main_Window", u"Propri\u00e9t\u00e9s du Graphique", None))
         self.label_3.setText(QCoreApplication.translate("Main_Window", u"Programme d'entretien actuelle :", None))
-        self.label_4.setText(QCoreApplication.translate("Main_Window", u"<html><head/><body><p>Task number :</p><p>P/N : </p><p>Type de suivi :</p><p>P\u00e9riodicit\u00e9 :</p></body></html>", None))
+        self.label_4.setText(QCoreApplication.translate("Main_Window", u"<html><head/><body><p>Num\u00e9ro de la t\u00e2che :</p><p>P/N : </p><p>Type de suivi :</p><p>P\u00e9riodicit\u00e9 :</p></body></html>", None))
         self.pushButton_3.setText(QCoreApplication.translate("Main_Window", u"Nouveau programme d'entretien", None))
         self.toolBox.setItemText(self.toolBox.indexOf(self.page_2), QCoreApplication.translate("Main_Window", u"Programme d'entretien", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("Main_Window", u"Total Occurency", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), QCoreApplication.translate("Main_Window", u"Occurency Leading To Part Remplacement", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("Main_Window", u"Linear Regression", None))
     # retranslateUi
 
